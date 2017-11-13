d7712001a9a1c2d61d508e69dc98ff463ff8a575
tsengwenbao <tsengwenbao@gmail.com>
2017-11-13 11:30:19 +0800

    go

---
+++ go.sh
@@ -0,0 +1,43 @@
+    1  kextcache
+    2  kextcache -u /
+    3  reboot
+    4  git
+    5  mkdir ~/Projects
+    6  cd  ~/Projects
+    7   git clone https://github.com/tsengwenbao/hp-840g1.git
+    8   git clone https://github.com/tsengwenbao/hp-840g1.git
+    9  ls -l
+   10  cd hp-840g1
+   11  ls -l
+   12  git check beta
+   13  git checkout beta
+   14  ./download.sh
+   15  ./install_downloads.sh
+   16  git clone https://github.com/Piker-Alpha/ssdtPRgrn.sh.git
+   17  git clone https://github.com/Piker-Alpha/ssdtPRGen.sh.git
+   18  ls -l
+   19  cd ssdtPRGen.sh
+   20  dir
+   21  ls
+   22  ./ssdtPRGen.sh
+   23  ls -l
+   24  ssdtPRGen.sh
+   25  cd ssdtPRGen.sh
+   26  ./ssdtPRGen.sh
+   27  sudo ./mount_efi.sh /
+   28  cd ..
+   29  sudo ./mount_efi.sh /
+   30  cd ~/Library/ssdtPRGen/ssdt.aml /Volumes/EFI/EFI/Clover/ACPI/pached/SSDT.aml
+   31  cp ~/Library/ssdtPRGen/ssdt.aml /Volumes/EFI/EFI/Clover/ACPI/pached/SSDT.aml
+   32  ls -l ~/Library/ssdtPRGen/ssdt.aml
+   33  ls -l  /Volumes/EFI/EFI/Clover/ACPI/patched
+   34  cp ~/Library/ssdtPRGen/ssdt.aml /Volumes/EFI/EFI/Clover/ACPI/patched/SSDT.aml
+   35  ./build.sh
+   36  ./install_acpi.sh help
+   37  ./install_acpi.sh install_8x0g1_haswell
+   38  ls  ./config
+   39  cp  ./config/config_8x0s_G1_Haswell.plist /Volumes/EFI/EFI/Clover/config.plist
+   40  sudo touch /System/Library/Extensions && sudo kextcache -u /
+   41  reboot
+   42  sudo reboot
+   43  history>good.txt
